93f8c719131d05684cf16fd179b36da47dd2e367
Tim Stirrat <tstirrat@google.com>
2018-09-21 12:16:03 -0700

    feat(a11y) App bar title text links to home

--- src/AppHeader.tsx
+++ src/AppHeader.tsx
@@ -9,9 +9,20 @@ import {
   TopAppBarTitle
 } from '@rmwc/top-app-bar';
 import * as React from 'react';
-import { withRouter } from 'react-router-dom';
+import styled from 'react-emotion';
+import { Link, withRouter } from 'react-router-dom';
 import onlyUpdateForKeys from 'recompose/onlyUpdateForKeys';
 import pure from 'recompose/pure';
+
+const HomeLink = styled(Link)`
+  color: var(--mdc-theme-on-secondary) !important;
+  text-decoration: none;
+
+  &:hover,
+  &:focus {
+    text-decoration: underline;
+  }
+`;
 
 export const AppHeader = pure(() => (
   <>
@@ -19,7 +30,10 @@ export const AppHeader = pure(() => (
       <TopAppBarRow tag="nav" role="navigation">
         <TopAppBarSection alignStart>
           <HomeIcon />
-          <TopAppBarTitle tag="h1">Peer ping</TopAppBarTitle>
+          <TopAppBarTitle tag="h1">
+            {/* TODO: <Theme wrap> types are broken */}
+            <HomeLink to="/">Peer ping</HomeLink>
+          </TopAppBarTitle>
         </TopAppBarSection>
       </TopAppBarRow>
     </TopAppBar>
